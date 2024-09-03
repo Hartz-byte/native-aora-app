69aad40df7a81933ddc70b9125acbb01f4ad8bc5
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-09-04 01:26:27 +0530

    working on index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white-500">
-      <Text className="text-3xl font-pblack">Aora!!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full h-full justify-center items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
 
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Home
-      </Link>
-    </View>
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
