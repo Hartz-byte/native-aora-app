d7c332ea90bffad315c667e99c457a42bcfea917
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-09-03 23:34:59 +0530

    file structure done

--- app/index.jsx
+++ app/index.jsx
@@ -7,6 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white-500">
       <Text className="text-3xl font-pbold">Aora!!</Text>
       <StatusBar style="auto" />
+
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
       </Link>
